45be02fd6ddfced848982954b7a7368391a5dd4c
Anna Timofeeva <padma@email.comanjatimofeyeva@gmail.com>
2017-06-09 16:25:16 -0700

    add refresh button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,25 @@ $(document).ready(function() {
       });
     });
 
+    //script for manually refreshing button
+    $("#btnRefresh").click(function() {
+         location.reload();
+    });
+
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script language="JavaScript">
+
+    $("#btnRefresh").click(function() {
+         location.reload();
+    });
+
+    </script>
+
+
+
+
+for (index=0; index <=numbersOfInt; index ++)    {
+
 
 if (number % 3 === 0){
   $("li").append("ping");
@@ -39,17 +58,18 @@ if (number % 3 === 0){
 } else if (number % 15 === 0){
   ("li").append("ping-pong");
   $("#result").show();
-} else if (numbersOfInt === 0){
+} else if (numbersOfInt === null){
   $("#error_mes_two").show();
-} else if {numbersOfInt === 
+} else if {numbersOfInt === ???????)//what if letters?????
+  $("#error_mes_two").show();
+
+} else{
+  $("ul").append(inputNumbersArr[index]);
+}
 
 }
-else
 
-
-
-
-
+//script for manually refreshing button
 
 
 
